cff91a10dfdb2adb157a3c9cc2ac70e2781cb1a3
proost <jwalag87@gmail.com>
2020-04-29 02:17:54 +0900

    git add dao layer and test

--- authserver/data_layer/rdb_manager.py
+++ authserver/data_layer/rdb_manager.py
@@ -6,44 +6,74 @@ from authserver.data_layer.data_access_sqls import (
 from authserver.resources.config import DB_HOST, DB_PASSWORD, DB_USER, DB_NAME
 
 
-class DbManager:
-    def __init__(self):
-        self.conn = pymysql.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            db=DB_NAME,
-            charset="utf8"
-        )
-    
-    def __enter__(self):
+class DbContextManagerBase:
+    def __init__(self, optional_connector=None):
+        if optional_connector:
+            self.conn = optional_connector
+        else:
+            self.conn = pymysql.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                db=DB_NAME,
+                charset="utf8",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
         self.cursor = self.conn.cursor()
-        return self.cursor
-    
+
+    def get_user_by_email(self, email):
+        try:
+            self.cursor.execute(GET_USER_BY_EMAIL, (email))
+            return self.cursor.fetchone()
+        except Exception as e:
+            print(e)
+            return None
+
+    def create_user(self, name, password, email):
+        try:
+            self.cursor.execute(CREATE_USER, (name, password, email))
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def change_user_by_email(self, name, password, email):
+        try:
+            self.cursor.execute(CHAGE_USER_BY_EMAIL, (name, password, email))
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+
+class DbContextManagerWithTxn(DbContextManagerBase):
+    def __init__(self, optional_connector=None, is_test=False):
+        super().__init__(optional_connector)
+        self.is_test = is_test
+
+    def __enter__(self):
+        return self
+
     def __exit__(self, *args, **kwargs):
-        self.conn.commit()
+        if self.is_test:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        
         self.cursor.close()
         self.conn.close()
 
 
-def get_user_by_email(email):
-    with DbManager() as mgr:
-        user = mgr.execute(GET_USER_BY_EMAIL, (email))
-        return user
+class DbContextManager(DbContextManagerBase):
+    def __init__(self, optional_connector=None):
+        super().__init__(optional_connector)
 
-def create_user(name, password, email):
-    with DbManager() as mgr:
-        try:
-            mgr.execute(CREATE_USER, (name, password, email))
-            return True
-        except Exception:
-            return False
+    def __enter__(self):
+        return self
 
-def change_user_by_email(name, password, email):
-    with DbManager() as mgr:
-        try:
-            mgr.execute(CHAGE_USER_BY_EMAIL, (name, password, email))
-            return True
-        except Exception:
-            return False
+    def __exit__(self, *args, **kwargs):      
+        self.cursor.close()
+        self.conn.close()
+
     
--- tests/data_layer/test_rdb_manager.py
+++ tests/data_layer/test_rdb_manager.py
@@ -1,16 +1,58 @@
 import pytest
+import pymysql
+
+from authserver.resources.config import DB_HOST, DB_PASSWORD, DB_USER, DB_NAME
 from authserver.data_layer.rdb_manager import (
-    get_user_by_email,
-    create_user,
-    change_user_by_email
+    DbContextManager, DbContextManagerWithTxn
 )
 
-def test_create_user():
-    username = 'tester2'
-    password = '1234'
-    email = 'tester2@example.com'
-    res = create_user(
-        name=username, password=password, email=email
+
+@pytest.fixture(scope="function")
+def generate_connector():
+    conn = pymysql.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        db=DB_NAME,
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor
     )
+    yield conn
+
+def test_create_user(generate_connector):
+    username = 'tester3'
+    password = '12345'
+    email = 'tester3@example.com'
+
+    conn = generate_connector
+    res = None
+    with DbContextManagerWithTxn(optional_connector=conn, is_test=True) as mgr:
+        res = mgr.create_user(username, password, email)
+    
     assert res
 
+def test_get_user_by_email(generate_connector):
+    email = "tester0@example.com"
+    expect = {
+        "email": "tester0@example.com",
+        "name": "tester0"
+    }
+
+    conn = generate_connector
+    res = None
+    with DbContextManager(optional_connector=conn) as mgr:
+        res = mgr.get_user_by_email(email)
+    
+    assert res == expect
+
+def test_change_user_by_email(generate_connector):
+    conn = generate_connector
+    email = "tester0@example.com"
+    password = "12345"    
+    name = "tester0"
+
+    res = None
+    with DbContextManagerWithTxn(optional_connector=conn, is_test=True) as mgr:
+        res = mgr.change_user_by_email(name, password, email)
+    
+    assert res
